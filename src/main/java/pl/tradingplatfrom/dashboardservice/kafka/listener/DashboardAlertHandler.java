a43f125cac924b930b3f1705c6d6eeaa7dd2c486
qbox <jakubprzydatek99@gmail.com>
2026-03-01 00:36:37 +0100

    dashboard alert handler and web socket config added

--- src/main/java/pl/tradingplatfrom/dashboardservice/kafka/listener/DashboardAlertHandler.java
+++ src/main/java/pl/tradingplatfrom/dashboardservice/kafka/listener/DashboardAlertHandler.java
@@ -1,4 +1,24 @@
 package pl.tradingplatfrom.dashboardservice.kafka.listener;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import pl.tradingplatform.events.AlertEvent;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class DashboardAlertHandler {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @KafkaListener(topics = "detected-alerts")
+    public void handleAlert(AlertEvent alert) {
+        log.info("Sending to WS: {} - {}", alert.getTicker(), alert.getDropMessage());
+        messagingTemplate.convertAndSend("/topic/alerts", alert);
+        messagingTemplate.convertAndSend("topic/alerts/" + alert.getTicker(), alert);
+    }
+
 }
